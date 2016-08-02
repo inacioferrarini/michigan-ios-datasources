3d4e7c063ca40a0a6abfd5d0f9075d1d38e1ea6c
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-08-02 14:11:35 -0300

    improved license and initial documentation.

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # michigan-ios-datasources
-
-[![CI Status](http://img.shields.io/travis/Inácio Ferrarini/michigan-ios-datasources.svg?style=flat)](https://travis-ci.org/Inácio Ferrarini/michigan-ios-datasources)
 [![Version](https://img.shields.io/cocoapods/v/michigan-ios-datasources.svg?style=flat)](http://cocoapods.org/pods/michigan-ios-datasources)
+[![Swift 3](https://img.shields.io/badge/Swift-3.0-orange.svg?style=flat)](https://swift.org/)
 [![License](https://img.shields.io/cocoapods/l/michigan-ios-datasources.svg?style=flat)](http://cocoapods.org/pods/michigan-ios-datasources)
+[![codecov.io](https://codecov.io/github/inacioferrarini/michigan-ios-datasources/coverage.svg?branch=master)](https://codecov.io/github/inacioferrarini/michigan-ios-datasources)
+
+[![CI Status](http://img.shields.io/travis/inacioferrarini/michigan-ios-datasources.svg?style=flat)](https://travis-ci.org/inacioferrarini/michigan-ios-datasources)
 [![Platform](https://img.shields.io/cocoapods/p/michigan-ios-datasources.svg?style=flat)](http://cocoapods.org/pods/michigan-ios-datasources)
+
+
+### What is it?
+
+michigan-ios-datasources provides a fluent builder block-based approach to UITableViewDelegate and UICollectionViewDelegate.
+
+
+### Goals
+
+* Dramatically reduce ViewControllers' size.
+* Provide a block-based approach to common situations.
+* Provide an easy project startup and reduce project startup time.
+* Make View Controller's code prettier, since it will no longer have to implement UITableViewDataSource or UICollectionViewDataSource itself.
+
 
 ## Example
 
@@ -26,4 +42,24 @@ Inácio Ferrarini, inacio.ferrarini@gmail.com
 
 ## License
 
-michigan-ios-datasources is available under the MIT license. See the LICENSE file for more info.
+The MIT License (MIT)
+
+Copyright (c) 2016 Inácio Ferrarini
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- michigan-ios-datasources/Classes/TableViewBlockDataSource.swift
+++ michigan-ios-datasources/Classes/TableViewBlockDataSource.swift
@@ -1,30 +1,63 @@
+//    The MIT License (MIT)
 //
-//  TableViewBlockDataSource.swift
-//  Pods
+//    Copyright (c) 2016 Inácio Ferrarini
 //
-//  Created by José Inácio Athayde Ferrarini on 02/08/16.
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
 //
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
 //
 
 import UIKit
 
+/** @class TableViewBlockDataSource
+ Provides a FLuent Interface / Fluent Builder Block-Based UITableViewDataSource implementation.
+  */
 public class TableViewBlockDataSource: NSObject {
 
+
+    // MARK: - Properties
+
+    /// The block used by func tableView:numberOfRowsInSection method
     public var numberOfRowsInSectionBlock: (tableView: UITableView, section: Int) -> Int
+    /// the block used by func tableView:cellForRowAtIndexPath method
     public var cellForRowAtIndexPathBlock: (tableView: UITableView, indexPath: NSIndexPath) -> UITableViewCell
+    /// the block used by func numberOfSectionsInTableView method
     public var numberOfSectionsInTableViewBlock: ((tableView: UITableView) -> Int)?
+    /// the block used by func tableView:titleForHeaderInSection method
     public var titleForHeaderInSectionBlock: ((tableView: UITableView, section: Int) -> String?)?
+    /// the block used by func tableView:titleForFooterInSection method
     public var titleForFooterInSectionBlock: ((tableView: UITableView, section: Int) -> String?)?
+    /// the block used by func tableView:canEditRowAtIndexPath method
     public var canEditRowAtIndexPathBlock: ((tableView: UITableView, indexPath: NSIndexPath) -> Bool)?
+    /// the block used by func tableView:canMoveRowAtIndexPath method
     public var canMoveRowAtIndexPathBlock: ((tableView: UITableView, canMoveRowAtIndexPath: NSIndexPath) -> Bool)?
+    /// the block used by func sectionIndexTitlesForTableView method
     public var sectionIndexTitlesForTableViewBlock: ((tableView: UITableView) -> [String]?)?
+    /// the block used by func tableView:sectionForSectionIndexTitle:atIndex method
     public var sectionForSectionIndexTitleBlock: ((tableView: UITableView, sectionForSectionIndexTitle: String, atIndex: Int) -> Int)?
+    /// the block used by func tableView:commitEditingStyle:forRowAtIndexPath method
     public var commitEditingStyleBlock: ((tableView: UITableView, commitEditingStyle: UITableViewCellEditingStyle, forRowAtIndexPath: NSIndexPath) -> Void)?
+    /// the block used by func tableView:moveRowAtIndexPath:toIndexPath method
     public var moveRowAtIndexPathBlock: ((tableView: UITableView, moveRowAtIndexPath: NSIndexPath, toIndexPath: NSIndexPath) -> Void)?
 
 
     // MARK: - Initialization
 
+    /// Initializes with the required properties.
     public init(numberOfRowsInSection: (tableView: UITableView, section: Int) -> Int,
                 cellForRowAtIndexPath: (tableView: UITableView, indexPath: NSIndexPath) -> UITableViewCell) {
         self.numberOfRowsInSectionBlock = numberOfRowsInSection
@@ -32,46 +65,55 @@ public class TableViewBlockDataSource: NSObject {
         super.init()
     }
 
+    /// Sets the numberOfSectionsInTableViewBlock and returns self. Used to chain initialization.
     public func withNumberOfSectionsBlock(numberOfSectionsInTableViewBlock: ((tableView: UITableView) -> Int)) -> Self {
         self.numberOfSectionsInTableViewBlock = numberOfSectionsInTableViewBlock
         return self
     }
 
+    /// Sets the titleForHeaderInSectionBlock and returns self. Used to chain initialization.
     public func withTitleForHeaderInSectionBlock(titleForHeaderInSectionBlock: ((tableView: UITableView, section: Int) -> String?)) -> Self {
         self.titleForHeaderInSectionBlock = titleForHeaderInSectionBlock
         return self
     }
 
+    /// Sets the titleForFooterInSectionBlock and returns self. Used to chain initialization.
     public func withTitleForFooterInSectionBlock(titleForFooterInSectionBlock: ((tableView: UITableView, section: Int) -> String?)) -> Self {
         self.titleForFooterInSectionBlock = titleForFooterInSectionBlock
         return self
     }
 
+    /// Sets the canEditRowAtIndexPathBlock and returns self. Used to chain initialization.
     public func withCanEditRowAtIndexPathBlock(canEditRowAtIndexPathBlock: ((tableView: UITableView, indexPath: NSIndexPath) -> Bool)) -> Self {
         self.canEditRowAtIndexPathBlock = canEditRowAtIndexPathBlock
         return self
     }
 
+    /// Sets the canMoveRowAtIndexPathBlock and returns self. Used to chain initialization.
     public func withCanMoveRowAtIndexPathBlock(canMoveRowAtIndexPathBlock: ((tableView: UITableView, canMoveRowAtIndexPath: NSIndexPath) -> Bool)) -> Self {
         self.canMoveRowAtIndexPathBlock = canMoveRowAtIndexPathBlock
         return self
     }
 
+    /// Sets the sectionIndexTitlesForTableViewBlock and returns self. Used to chain initialization.
     public func withSectionIndexTitlesForTableViewBlock(sectionIndexTitlesForTableViewBlock: ((tableView: UITableView) -> [String]?)) -> Self {
         self.sectionIndexTitlesForTableViewBlock = sectionIndexTitlesForTableViewBlock
         return self
     }
 
+    /// Sets the sectionForSectionIndexTitleBlock and returns self. Used to chain initialization.
     public func withSectionForSectionIndexTitleBlock(sectionForSectionIndexTitleBlock: ((tableView: UITableView, sectionForSectionIndexTitle: String, atIndex: Int) -> Int)) -> Self {
         self.sectionForSectionIndexTitleBlock = sectionForSectionIndexTitleBlock
         return self
     }
 
+    /// Sets the commitEditingStyleBlock and returns self. Used to chain initialization.
     public func withCommitEditingStyleBlock(commitEditingStyleBlock: ((tableView: UITableView, commitEditingStyle: UITableViewCellEditingStyle, forRowAtIndexPath: NSIndexPath) -> Void)) -> Self {
         self.commitEditingStyleBlock = commitEditingStyleBlock
         return self
     }
 
+    /// Sets the moveRowAtIndexPathBlock and returns self. Used to chain initialization.
     public func withMoveRowAtIndexPathBlock(moveRowAtIndexPathBlock: ((tableView: UITableView, moveRowAtIndexPath: NSIndexPath, toIndexPath: NSIndexPath) -> Void)) -> Self {
         self.moveRowAtIndexPathBlock = moveRowAtIndexPathBlock
         return self
@@ -84,6 +126,7 @@ extension TableViewBlockDataSource: UITableViewDataSource {
 
     // MARK: - DataSource Implementation
 
+    /// Will return the value of numberOfRowsInSectionBlock execution.
     public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.numberOfRowsInSectionBlock(tableView: tableView, section: section)
     }
@@ -91,20 +134,24 @@ extension TableViewBlockDataSource: UITableViewDataSource {
 
     // MARK: - Row Display
 
+    /// Will return the value of cellForRowAtIndexPathBlock execution.
     public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         return self.cellForRowAtIndexPathBlock(tableView: tableView, indexPath: indexPath)
     }
 
+    /// Will return the value of numberOfSectionsInTableViewBlock execution. If not defined, will return 1.
     public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         guard let block = self.numberOfSectionsInTableViewBlock else { return 1 }
         return block(tableView: tableView)
     }
 
+    /// Will return the value of titleForHeaderInSectionBlock execution. If not defined, will return nil.
     public func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         guard let block = self.titleForHeaderInSectionBlock else { return nil }
         return block(tableView: tableView, section: section)
     }
 
+    /// Will return the value of titleForFooterInSectionBlock execution. If not defined, will return nil.
     public func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
         guard let block = self.titleForFooterInSectionBlock else { return nil }
         return block(tableView: tableView, section: section)
@@ -113,6 +160,7 @@ extension TableViewBlockDataSource: UITableViewDataSource {
 
     // MARK: - Editing
 
+    /// Will return the value of canEditRowAtIndexPathBlock execution. If not defined, will return true.
     public func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         guard let block = self.canEditRowAtIndexPathBlock else { return true }
         return block(tableView: tableView, indexPath: indexPath)
@@ -121,16 +169,19 @@ extension TableViewBlockDataSource: UITableViewDataSource {
 
     // MARK: - Moving/reordering
 
+    /// Will return the value of canMoveRowAtIndexPathBlock execution. If not defined, will return false.
     public func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         guard let block = self.canMoveRowAtIndexPathBlock else { return false }
         return block(tableView: tableView, canMoveRowAtIndexPath: indexPath)
     }
 
+    /// Will return the value of sectionIndexTitlesForTableViewBlock execution. If not defined, will return nil.
     public func sectionIndexTitlesForTableView(tableView: UITableView) -> [String]? {
         guard let block = self.sectionIndexTitlesForTableViewBlock else { return nil }
         return block(tableView: tableView)
     }
 
+    /// Will return the value of sectionForSectionIndexTitleBlock execution. If not defined, will return 0.
     public func tableView(tableView: UITableView, sectionForSectionIndexTitle title: String, atIndex index: Int) -> Int {
         guard let block = self.sectionForSectionIndexTitleBlock else { return 0 }
         return block(tableView: tableView, sectionForSectionIndexTitle: title, atIndex: index)
@@ -139,11 +190,13 @@ extension TableViewBlockDataSource: UITableViewDataSource {
 
     // MARK: - Data manipulation - insert and delete support
 
+    /// Will execute the block commitEditingStyleBlock, if defined.
     public func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         guard let block = self.commitEditingStyleBlock else { return }
         block(tableView: tableView, commitEditingStyle: editingStyle, forRowAtIndexPath: indexPath)
     }
 
+    /// Will execute the block moveRowAtIndexPathBlock, if defined.
     public func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
         guard let block = self.moveRowAtIndexPathBlock else { return }
         block(tableView: tableView, moveRowAtIndexPath: sourceIndexPath, toIndexPath: destinationIndexPath)
